ceb2e52e74685ece1c7ca8f987a120d7f0d81053
minaitakura <minaitakura37@gmail.com>
2016-03-26 05:03:55 +0000

    user's profile edit function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
 
-  def show # 追加
+  def show 
    @user = User.find(params[:id])
   end
 
@@ -18,10 +19,28 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Edited Profile!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :region)
   end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
 end
